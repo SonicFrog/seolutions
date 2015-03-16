382ce5ebd3d43ccce15828358949c6333527dc05
Ogier Bouvier <ogier.bouvier@epfl.ch>
2015-03-17 00:04:04 +0100

    New urls for by site report viewing

--- google/urls.py
+++ google/urls.py
@@ -4,13 +4,23 @@ from google import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.ReportIndexView.as_view(),
-                   name="report-index"),
-    url(r'^(?P<pk>\d+)/$', views.ReportDetailView.as_view(),
-                name='report-detail'),
-    url(r'^(?P<pk>\d+)/delete$', views.ReportDeleteView.as_view(),
-                   name="report-delete"),
-    url(r'^(?P<first_report_id>\d+)/(?P<second_report_id>\d+)/$',
-                   views.ReportCompareView.as_view(), name="report-compare"),
+                       url(r'^$', views.ReportIndexView.as_view(),
+                           name="report-index"),
 
+                       url(r'^(?P<pk>\d+)/$', views.ReportDetailView.as_view(),
+                           name='report-detail'),
+
+                       url(r'^(?P<pk>\d+)/delete$',
+                           views.ReportDeleteView.as_view(),
+                           name="report-delete"),
+
+                       url(r'^(?P<first_report_id>\d+)/(?P<second_report_id>\d+)/$',
+                           views.ReportCompareView.as_view(), name="report-compare"),
+
+                       url(r'^bysite/$', views.SiteIndexView.as_view(),
+                           name='site-index'),
+
+                       url(r'^bysite/(?P<pk>\d+)$',
+                           views.SiteDetailView.as_view(),
+                           name='site-view'),
 )
